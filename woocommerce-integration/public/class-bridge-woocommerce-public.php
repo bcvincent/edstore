f2b6b0d814aae5b91f8982b1818fa7436fb38a70
Brian Vincent <bcvincent@gmail.com>
2018-01-24 13:55:00 -0500

    removing code that adds courses to product list, we don't need this.

--- woocommerce-integration/public/class-bridge-woocommerce-public.php
+++ woocommerce-integration/public/class-bridge-woocommerce-public.php
@@ -181,30 +181,7 @@ namespace NmBridgeWoocommerce{
         public function groupedProductDisplayAssociatedCourses($product)
         {
             $product_options = get_post_meta($product->get_id(), 'product_options', true);
-            if (isset($product_options['moodle_post_course_id']) && is_array($product_options['moodle_post_course_id']) && ! empty($product_options['moodle_post_course_id'])) {
-                ob_start();
-                ?>
-                <td>
-                    <div class="wi-asso-courses-wrapper">
-                <h7><?php _e('Courses', WOOINT_TD); ?></h7>
-                <ul class="bridge-woo-available-courses">
-                    <?php
-                    foreach ($product_options['moodle_post_course_id'] as $single_course_id) {
-                        if ('publish' === get_post_status($single_course_id)) {
-                            ?>
-                            <li>
-                                <a href="<?php echo esc_url(get_permalink($single_course_id)); ?>" target="_blank"><?php echo get_the_title($single_course_id); ?></a>
-                            </li>
-                            <?php
-                        }
-                    }
-                    ?>
-                </ul>
-            </div>
-                </td>
-                <?php
-                echo ob_get_clean();
-            }
+          
             // echo '<td>';
             // echo esc_html(do_shortcode('[bridge_woo_display_associated_courses product_id='.$product->get_id().']'));
             // echo '</td>';
